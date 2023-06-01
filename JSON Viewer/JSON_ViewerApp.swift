82e262c7db9779fe40f64c02239776cb6541afee
Aditya Dhar <adhar2032@gmail.com>
2023-06-01 10:55:13 -0400

    Initial Commit

---
+++ JSON Viewer/JSON_ViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  JSON_ViewerApp.swift
+//  JSON Viewer
+//
+//  Created by Aditya Dhar on 6/1/23.
+//
+
+import SwiftUI
+
+@main
+struct JSON_ViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
