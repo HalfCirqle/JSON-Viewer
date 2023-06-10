260752bffbd8d54eedeb2c657085463f4067d970
Aditya Dhar <adhar2032@gmail.com>
2023-06-10 09:53:19 -0400

    Created notification names

---
+++ JSON Viewer/Notification+Name.swift
@@ -0,0 +1,13 @@
+//
+//  Notification+Name.swift
+//  JSON Viewer
+//
+//  Created by Aditya Dhar on 6/10/23.
+//
+
+import Foundation
+
+extension Notification.Name {
+    static let expandAll = Notification.Name("expandAll")
+    static let closeAll = Notification.Name("closeAll")
+}
