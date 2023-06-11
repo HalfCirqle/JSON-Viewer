77ff53832b4ba538b96063cf09bc40f48f079c25
Aditya Dhar <adhar2032@gmail.com>
2023-06-11 08:53:21 -0400

    Added opening and closing of hcJSON documents

--- JSON Viewer/Project Files/JSON_ViewerApp.swift
+++ JSON Viewer/Project Files/JSON_ViewerApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct JSON_ViewerApp: App {
     var body: some Scene {
         DocumentGroup(newDocument: Document()) { file in
-            ContentView()
+            ContentView(document: file.$document)
         }
     }
 }
