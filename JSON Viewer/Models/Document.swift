108a3a5786cc80a48e962aa44697faa992a7b1f8
Aditya Dhar <adhar2032@gmail.com>
2023-06-11 09:09:08 -0400

    Added documentation to Document struct

--- JSON Viewer/Models/Document.swift
+++ JSON Viewer/Models/Document.swift
@@ -8,13 +8,35 @@
 import SwiftUI
 import UniformTypeIdentifiers
 
+/**
+ A `FileDocument` compliant struct representing a document in the application.
+ The `Document` struct is responsible for maintaining and serializing the primary data model for the application. This includes a single `Node` object, which represents the root of a tree of `Node` objects.
+ - Important: This struct must be initialized with a `Node`.
+ It also includes metadata about the document, including its version, creation date, modification date, and comments.
+ - Note: The document version is initially `1` and can be incremented as needed.
+ The `Document` struct conforms to the `FileDocument` protocol, which means it can be used with SwiftUI's document-based app architecture. It provides methods for reading documents from and writing documents to disk.
+ */
 struct Document: FileDocument {
+
+    /// The root node of the document.
     var node: Node?
+
+    /// The version of the document. This is initially `1`.
     var version: Int
+
+    /// The date the document was created.
     var createdAt: Date
+
+    /// The date the document was last modified.
     var modifiedAt: Date
+
+    /// Any comments about the document.
     var comments: String
 
+    /**
+     Creates a new `Document`.
+     - Parameter node: The root node of the document. This defaults to `nil`.
+     */
     init(node: Node? = nil) {
         self.node = node
         self.version = 1
@@ -23,10 +45,14 @@ struct Document: FileDocument {
         self.comments = ""
     }
 
-    // define readable content types for your document
+    /// The readable content types for the document.
     static var readableContentTypes: [UTType] { [.jsonCommentedDocument] }
 
-    // initialize your document with a file
+    /**
+     Initializes a new `Document` from a `ReadConfiguration`.
+     - Parameter configuration: The `ReadConfiguration` to initialize from.
+     - Throws: An error if the document data is corrupt or cannot be read.
+     */
     init(configuration: ReadConfiguration) throws {
         guard let data = configuration.file.regularFileContents,
               let decoded = try? JSONDecoder().decode(Document.self, from: data)
@@ -36,7 +62,12 @@ struct Document: FileDocument {
         self = decoded
     }
 
-    // serialize your document to data to save it
+    /**
+     Returns a `FileWrapper` that represents the document's current state.
+     - Parameter configuration: A `WriteConfiguration` to write with.
+     - Throws: An error if the document's contents cannot be encoded.
+     - Returns: A `FileWrapper` containing the encoded document.
+     */
     func fileWrapper(configuration: WriteConfiguration) throws -> FileWrapper {
         let data = try JSONEncoder().encode(self)
         return .init(regularFileWithContents: data)
@@ -44,10 +75,17 @@ struct Document: FileDocument {
 }
 
 extension Document: Codable {
+
+    /// An enumeration of the coding keys used to encode and decode a `Document`.
     enum CodingKeys: CodingKey {
         case node, version, createdAt, modifiedAt, comments
     }
-    
+
+    /**
+     Initializes a new `Document` from a decoder.
+     - Parameter decoder: The `Decoder` to decode data from.
+     - Throws: An error if the document cannot be decoded.
+     */
     init(from decoder: Decoder) throws {
         let values = try decoder.container(keyedBy: CodingKeys.self)
         node = try values.decode(Node.self, forKey: .node)
@@ -56,7 +94,12 @@ extension Document: Codable {
         modifiedAt = try values.decode(Date.self, forKey: .modifiedAt)
         comments = try values.decode(String.self, forKey: .comments)
     }
-    
+
+    /**
+     Encodes the `Document` to an encoder.
+     - Parameter encoder: The `Encoder` to encode data to.
+     - Throws: An error if the document cannot be encoded.
+     */
     func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
         try container.encode(node, forKey: .node)
@@ -68,6 +111,7 @@ extension Document: Codable {
 }
 
 extension UTType {
+    /// A UTType that identifies the custom document type for this application.
     static var jsonCommentedDocument: UTType {
         UTType(exportedAs: "com.halfcirqle.json-commented")
     }
